b2de275bed0ef6756863fbed38adab5d2e0dd1d0
Aryash Saxena <124175879+aryashsaxena@users.noreply.github.com>
2023-06-30 00:46:57 +0530

    Time: 74 ms (8.74%) | Memory: 16.5 MB (12.72%) - LeetSync

--- stone-game/stone-game.cpp
+++ stone-game/stone-game.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int n;
 
-    int f(int i,int j,int x,vector<int>&nums,vector<vector<vector<int>>>&dp){
+    int f(int i,int j,int x,vector<int>&nums,vector<vector<int>>&dp){
         if(i>=n || j<0 ) return 0;
-        if(dp[i][j][x]!=-1) return dp[i][j][x];
+        if(dp[i][j]!=-1) return dp[i][j];
 
         int take=0,ntake=0;
         if(x==0){
@@ -17,12 +17,12 @@ public:
         }
 
         // cout<<take<<" "<<ntake<<endl;
-        return dp[i][j][x]= max(take,ntake);
+        return dp[i][j]= max(take,ntake);
     }
 
     bool stoneGame(vector<int>& piles) {
          n=piles.size();
-       vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(n+1,vector<int>(2,-1)));
+       vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
 
         int x=f(0,n-1,0,piles,dp) ;
         cout<<x<<endl;
