f7972deae382ab95eed41214654d8f8bb2ebd2e8
Aryash Saxena <124175879+aryashsaxena@users.noreply.github.com>
2023-06-30 20:15:05 +0530

    Time: 12 ms (50.28%) | Memory: 17.4 MB (38.92%) - LeetSync

--- maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -11,21 +11,31 @@
  */
 class Solution {
 public:
-    unordered_map<int,unsigned long long> dp;
-
-    unsigned long long f(TreeNode* root,int l,unsigned long long idx){
-        if(!root) return 0;
-        
-        if(!dp.count(l)) dp[l]=idx;
-
-        unsigned long long left=f(root->left,l+1,(1LL*2*idx));
-        unsigned long long right=f(root->right,l+1,(1LL*2*idx)+1);
-        unsigned long long ans=idx-dp[l]+1;
-        return max({ans,left,right});
-    }
-
     int widthOfBinaryTree(TreeNode* root) {
-       
-        return f(root,0,0);
+        if(!root) return 0;
+
+        queue<pair<TreeNode*,long long>> q;
+        q.push({root,1});
+        int ans=0;
+
+        while(!q.empty()){
+            int sz=q.size();
+            int mn=q.front().second;
+            int fst=0,lst;
+
+            while(sz--){
+                auto it=q.front();
+                q.pop();
+
+                int cur=it.second-mn;
+
+                if(sz==0) lst=cur;
+                if(it.first->left) q.push({it.first->left,1LL*2*cur});
+                if(it.first->right) q.push({it.first->right,1LL*2*cur+1});
+            }
+
+            ans=max(ans,lst-fst+1);
+        }
+        return ans;
     }
 };
