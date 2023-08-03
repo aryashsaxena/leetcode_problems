4a187386514651ee5cdc7fba96540c0b749fb11c
Aryash Saxena <124175879+aryashsaxena@users.noreply.github.com>
2023-08-03 09:51:25 +0530

    Time: 3 ms (37.56%) | Memory: 6.5 MB (55.27%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    vector<string> ans;
     unordered_map<char,string> m;
+    vector<string> ans;
 
-
-    void f(int i,string &s,string &str){
-        if(i==str.size()){
+    void  f(int i,string &s,string dgt){
+        if(i>=dgt.size()){
             ans.push_back(s);
             return;
         }
 
-        string t=m[str[i]];
+        string t=m[dgt[i]];
         for(int j=0;j<t.size();j++){
             s.push_back(t[j]);
-            f(i+1,s,str);
+            f(i+1,s,dgt);
             s.pop_back();
         }
     }
 
     vector<string> letterCombinations(string digits) {
         if(digits.size()==0) return ans;
-    
-        m.insert({'2',"abc"});
-        m.insert({'3',"def"});
-         m.insert({'4',"ghi"});
-         m.insert({'5',"jkl"});
-         m.insert({'6',"mno"});
-         m.insert({'7',"pqrs"});
-         m.insert({'8',"tuv"});
-         m.insert({'9',"wxyz"});   
 
-         string s="";
-         f(0,s,digits);
-         return ans;
+        m['2']="abc";
+        m['3']="def";
+        m['4']="ghi";
+        m['5']="jkl";
+        m['6']="mno";
+        m['7']="pqrs";
+        m['8']="tuv";
+        m['9']="wxyz";
+        
+        string s="";
+        f(0,s,digits);
+
+        return ans;
     }
 };
