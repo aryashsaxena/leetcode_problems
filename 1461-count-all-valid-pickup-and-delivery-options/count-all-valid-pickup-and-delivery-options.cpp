ce2b6879496683e80fdb72df597c2ea900d84368
Aryash Saxena <124175879+aryashsaxena@users.noreply.github.com>
2023-09-10 13:23:15 +0530

    Time: 46 ms (6.76%) | Memory: 30.7 MB (5.48%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    long long mod=1e9+7;
-    long long f(long long i,long long n,vector<long long>&dp){
-        if(i==n) return 1;
-        if(dp[i]!=-1) return dp[i];
+    int mod=1e9+7;
+    
+    long long f(long long i,long long j,vector<vector<long long>>&dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0 || i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
 
-        long long ans=((n-i)*(2*(n-i)-1))%mod;
-        ans=((ans%mod)*(f(i+1,n,dp)%mod))%mod;
-        return dp[i]=ans;
+        long long ans=((i%mod)*(f(i-1,j,dp)%mod))%mod;
+        ans=ans%mod + (((j-i)%mod)*(f(i,j-1,dp)%mod))%mod;
+        return dp[i][j]=ans;
     }
 
     int countOrders(int n) {
-       vector<long long> dp(n+1,-1);
-       return f(0,n,dp);
+        vector<vector<long long>>dp(n+1,vector<long long>(n+1,-1));
+        return f(n,n,dp);
     }
 };
