0b5e82a0bea89a5afe19d49c7031620746a5351a
Aryash Saxena <124175879+aryashsaxena@users.noreply.github.com>
2023-10-25 22:40:09 +0530

    Time: 33 ms (79.81%) | Memory: 24.9 MB (14.31%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -2,16 +2,32 @@ class Solution {
 public:
     int longestCommonSubsequence(string s, string t) {
         vector<vector<int>> dp(s.size()+1,vector<int>(t.size()+1,0));
+        int mx=max(s.size(),t.size());
+        vector<int> prev(mx+1,0),cur(mx+1,0);
 
         for(int i=1;i<=s.size();i++){
             for(int j=1;j<=t.size();j++){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    cur[j]=1+prev[j-1];
                 }
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                else cur[j]=max(prev[j],cur[j-1]);
             }
+            prev=cur;
         }
 
-        return dp[s.size()][t.size()];
+        // int i=s.size(),j=t.size();
+        // string ans="";
+
+        // while(i>0 && j>0){
+        //     if(s[i-1]==t[j-1]){
+        //         ans=s[i-1]+ans;
+        //         i--;
+        //         j--;
+        //     }
+        //     else if(dp[i-1][j]>dp[i][j-1]) i--;
+        //     else j--;
+        // }
+        // cout<<ans<<endl;
+        return prev[t.size()];
     }
 };
